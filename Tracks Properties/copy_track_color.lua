76aff43cddc739a700663be59f20b3d1fe3a79da
Wuzzmi <jamref@gmail.com>
2025-05-09 12:34:18 -0700

    Update copy_track_color.lua
    Should work properly on any OS now.

--- Tracks Properties/copy_track_color.lua
+++ Tracks Properties/copy_track_color.lua
@@ -3,22 +3,25 @@
 -- @version 1.0
 -- @about Copies the selected track's color as hex to clipboard. The hex format used is "0xRRGGBB". Made for translating track colors to sws auto color.
 
-
 -- Get track color
-local track = reaper.GetSelectedTrack( 0, 0 )
-local deci_color = reaper.GetTrackColor( track )
+local track = reaper.GetSelectedTrack(0, 0)
+local deci_color = reaper.GetTrackColor(track)
 
 -- decimal > hex
 local hex = string.format("%x", deci_color):sub(2)
-local red = string.sub( hex, 5, 6 )
-local green = string.sub( hex, 3, 4 )
-local blue = string.sub( hex, 1, 2 )
-local conv_hex = red .. green .. blue
+--check os
+local os = package.config:sub(1,1)
+--if windows reorder colors
+if os == "\\" then
+    local red = string.sub(hex, 5, 6)
+    local green = string.sub(hex, 3, 4)
+    local blue = string.sub(hex, 1, 2)
+    hex = red .. green .. blue
+end
 
--- Add prefix 
+-- Add prefix
 local pre = "0x"
-local formated_hex = pre .. conv_hex
+local formated_hex = pre .. hex
 
 -- Copy > clipboard
-reaper.CF_SetClipboard( formated_hex )
-
+reaper.CF_SetClipboard(formated_hex)
